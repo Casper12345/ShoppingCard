fcb8549419c49bb1cfeabc710535c7912f4db971
Casper12345 <caspertellerup@hotmail.com>
2017-10-23 16:23:59 +0100

    Third test added

--- src/test/scala/ShoppingCartTest.scala
+++ src/test/scala/ShoppingCartTest.scala
@@ -17,4 +17,32 @@ class ShoppingCartTest extends FunSuite {
 
   }
 
+  /**
+    * Second TDD test for creating checkout.
+    */
+  test("testShoppingCart2") {
+
+    val sC = new ShoppingCart
+
+    val list = List[Fruit](Orange, Orange, Orange, Orange, Orange)
+
+    assert(sC.checkOut(list) == 5 * 0.25)
+
+
+  }
+
+  /**
+    * Third TDD test for creating checkout.
+    */
+  test("testShoppingCart3") {
+
+    val sC = new ShoppingCart
+
+    val list = List[Fruit](Apple, Apple, Orange, Apple)
+
+    assert(sC.checkOut(list) == (3 * 0.60) + (1 * 0.25))
+
+
+  }
+
 }
